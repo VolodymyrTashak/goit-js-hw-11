3b0b26bbf983686b894da8f3ed0bea548e762ef1
VolodymyrTashak <vovavozny33@gmail.com>
2022-09-13 23:44:55 +0300

    1

---
+++ src/js/index.js
@@ -0,0 +1,87 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import debounce from 'lodash.debounce';
+import { NewsApiService } from './news-service';
+import { getRefs } from './getRefs';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const refs = getRefs();
+const newsApiService = new NewsApiService();
+
+refs.form.addEventListener('submit', onSearch);
+refs.loadMoreBtn.addEventListener('click', onLoadMore);
+
+async function onSearch(e) {
+  e.preventDefault();
+
+  newsApiService.query = e.currentTarget.elements.searchQuery.value.trim();
+  if (newsApiService.query === '') {
+    return Notify.failure('Please write what to look for');
+  }
+  newsApiService.resetPage();
+
+  await newsApiService.fetchArticles().then(res => {
+    if (res.hits.length === 0) {
+      Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+    }
+    clearGallery();
+    Notify.success(`Hooray! We found ${res.totalHits} images.`);
+    renderGallery(res.hits);
+    if (res.totalHits <= 40) {
+      return Notify.failure(
+        "We're sorry, but you've reached the end of search results."
+      );
+    } else {
+      refs.loadMoreBtn.classList.remove('is-hidden');
+    }
+  });
+}
+
+async function onLoadMore(e) {
+  await newsApiService.fetchArticles().then(res => {
+    renderGallery(res.hits);
+  });
+}
+
+function clearGallery() {
+  refs.gallery.innerHTML = '';
+}
+
+function renderGallery(data) {
+  const render = data
+    .map(
+      ({
+        largeImageURL,
+        webformatURL,
+        likes,
+        views,
+        comments,
+        downloads,
+        tags,
+      }) => `<div class='card'>
+    <a class="card-item" href="${largeImageURL}">
+    <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+    </a>
+    <div class="info">
+      <p class="info-item">
+        <b>Likes</b> ${likes}
+      </p>
+      <p class="info-item">
+        <b>Views</b> ${views}
+      </p>
+      <p class="info-item">
+        <b>Comments</b>${comments}
+      </p>
+      <p class="info-item">
+        <b>Downloads</b> ${downloads}
+      </p>
+    </div>
+    </div>`
+    )
+    .join('');
+  refs.gallery.insertAdjacentHTML(`beforebegin`, render);
+  let lightbox = new SimpleLightbox('.gallery a');
+  lightbox.refresh();
+}
---
+++ src/js/news-service.js
@@ -0,0 +1,48 @@
+import axios from 'axios';
+
+export default class NewsApiService {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+  }
+
+  async fetchArticles() {
+    const BASE_URL = 'https://pixabay.com/api/';
+    const options = {
+      key: '29860210-f6d08db11b6c43066ac2ccb28',
+      q: `${this.searchQuery}`,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+      per_page: 40,
+      page: `${this.page}`,
+    };
+
+    try {
+      const response = await axios
+        .get(BASE_URL, { options })
+        .then(res => {
+          this.incrementPage();
+          return res.data;
+        })
+        .then(console.log);
+      return response;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  incrementPage() {
+    this.page += 1;
+  }
+  resetPage() {
+    this.page = 1;
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+}
